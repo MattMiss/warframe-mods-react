e62078a358d4d4c7e75c1fe7955d0311f66852fe
Matt Miss <Matt.w.miss@gmail.com>
2020-01-26 00:36:03 -0800

    works with warfram-mods

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import '../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
